6bb305285ba47633f9f1233b2f5a817ce8479a0f
Aadarsh Mishra <aadarsh9600@gmail.com>
2022-05-01 01:36:05 +0530

    DB setup.

---
+++ src/test/java/com/iiitb/instilunchbox/InstiLunchboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.instilunchbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstiLunchboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
