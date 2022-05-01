7cddba3418a00f500e458c9130d2c36d116d8d31
Aadarsh Mishra <aadarsh9600@gmail.com>
2022-05-02 01:46:25 +0530

    User model and repo.

---
+++ src/main/java/com/iiitb/instilunchbox/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.iiitb.instilunchbox.Repository;
+
+import com.iiitb.instilunchbox.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository {
+    Optional<User> findUserByEmail(String email);
+}
